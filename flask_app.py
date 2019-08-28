145525872d1df91512762efcb5fb18f6555a2277
Tomas Ortega <torte007@fiu.edu>
2019-08-28 04:21:22 -0400

    adding base template

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,11 @@
-from flask import Flask 
+from flask import Flask, render_template
+from flask-bootstrap import Bootstrap
 app = Flask(__name__)
+bootstrap = Bootstrap(app) 
 
 @app.route('/')
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
